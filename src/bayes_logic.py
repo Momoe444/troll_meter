3b7609bbae68f0322f33bb8208e924257884f094
Manuel_Sambrano_Mendoza <25460335@colima.tecmn.mx>
2025-12-04 15:46:21 -0600

    Primer Commit

--- src/bayes_logic.py
+++ src/bayes_logic.py
@@ -2,35 +2,35 @@ import json
 import os
 
 class TrollBrain:
-    def __init__(self, data_path="data/memoria_ia.jsom"):
+    def __init__(self, data_path="data/memoria_ia.json"):
         self.data_path = data_path
-        #Dos diccionarios: Clave=Palabra, Valor=Frecuencia
+        # Dos diccionarios: Clave=Palabra, Valor=Frecuencia
         self.vocab_toxico = {}
         self.vocab_pro = {}
         self.cargar_memoria()
     
     def aprender(self, frase, categoria):
-        """Descompone la frase y guarda las palabras en la categoría correspondiente"""
-        palabras =  frase.lower().split()
+        """Descompone la frase y guarda las palabras en la categoria correcta"""
+        palabras = frase.lower().split()
 
         for palabra in palabras:
-            #Filtro básico: ignorar palabreas de menos de 2 letras
+            # Filtro basico: ignorar palabras de menos de 2 letras
             if len(palabra) > 2:
                 if categoria == "toxico":
-                    #Si existe suma1, si no existe empieza en 0 y suma 1
+                    # Si existe suma 1, si no existe empieza en 0 y suma 1
                     self.vocab_toxico[palabra] = self.vocab_toxico.get(palabra, 0) + 1
                 else:
                     self.vocab_pro[palabra] = self.vocab_pro.get(palabra, 0) + 1
 
-        #Guardamos en disco para no olvidar al reiniciar
+        # Guardamos en disco para no olvidar al reiniciar
         self.guardar_memoria()
-
+    
     def predecir(self, frase):
-        """Calcula los puntos para cada bando (Algoritmo Naive Bayes Simplificado)"""
+        """"Calcula los puntos para cada bando (Algoritmo Naive Bayes Simplificado)"""
         score_toxico = 0
         score_pro = 0
-        detalles = [] #Para explicar por que tomo la decision
-        palabras = frase.lower().split
+        detalles = [] # Para explicar por que tomo la decision
+        palabras = frase.lower().split()
 
         for palabra in palabras:
             pt = self.vocab_toxico.get(palabra, 0)
@@ -40,7 +40,7 @@ class TrollBrain:
 
             if pt > 0 or pp > 0:
                 detalles.append(f"'{palabra}' (T:{pt} / A:{pp})")
-            
+
         return score_toxico, score_pro, detalles
     
     def guardar_memoria(self):
@@ -52,7 +52,7 @@ class TrollBrain:
         os.makedirs(os.path.dirname(self.data_path), exist_ok=True)
         with open(self.data_path, 'w') as f:
             json.dump(datos, f)
-    
+
     def cargar_memoria(self):
         if os.path.exists(self.data_path):
             with open(self.data_path, 'r') as f:
